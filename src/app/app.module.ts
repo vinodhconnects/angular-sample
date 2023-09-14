f9b319df159bacdca88e38dff531605e4b5cc024
vinodhconnects <vinodhconnects@gmail.com>
2023-09-14 17:22:04 +0530

    code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CircleComponent } from './circle/circle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CircleComponent //component,directives and pipes
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule //inlude third party modules or any user defined module
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],//services
+  bootstrap: [AppComponent]//components that can be called in index.html
 })
 export class AppModule { }
